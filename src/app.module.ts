10d1418fce91898f16fc1c83c31785aa48b19590
prathamesh patil <prathm.patil.100@gmail.com>
2024-03-21 02:24:55 +0530

    feat: adding database and config support

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_URL),
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+  ],
 })
 export class AppModule {}
